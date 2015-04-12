a7b7b39e76639bf4124fcbf83f22e05611ac626a
dom767 <dominicpenfold@gmail.com>
2015-04-12 22:57:38 +0100

    Transparent Cylinder support
    Backface support

--- CoreTracer/CoreTracer/Code/RenderCylinder.cpp
+++ CoreTracer/CoreTracer/Code/RenderCylinder.cpp
@@ -29,7 +29,7 @@ bool DRenderCylinder::InternalIntersect(const DRayContext& rRayContext, DCollisi
 	float b24ac = b*b - 4*a*c;
 	if (b24ac<0)
 		return false;
-	
+
 	float sqb24ac = sqrtf(b24ac);
 	t0 = (-b + sqb24ac) / (2 * a);
 	t1 = (-b - sqb24ac) / (2 * a);
@@ -39,44 +39,90 @@ bool DRenderCylinder::InternalIntersect(const DRayContext& rRayContext, DCollisi
 	float y0 = start.mComponent[1] + t0 * direction.mComponent[1];
 	float y1 = start.mComponent[1] + t1 * direction.mComponent[1];
 
-	if (y0<-1)
+	if ((rRayContext.m_RequestFlags&RequestBackface)==0)
 	{
-		if (y1<-1)
-			return false;
-		else
+		if (y0<-1)
 		{
-			// hit the cap
-			float th = t0 + (t1-t0) * (y0+1) / (y0-y1);
-			if (th<=0) return false;
+			if (y1<-1)
+				return false;
+			else
+			{
+				// hit the cap
+				float th = t0 + (t1-t0) * (y0+1) / (y0-y1);
+				if (th<=0) return false;
 		
-			out_Response.mHitPosition = start + (direction*th);
-			out_Response.mNormal = DVector3(0, -1, 0);
+				out_Response.mHitPosition = start + (direction*th);
+				out_Response.mNormal = DVector3(0, -1, 0);
+				return true;
+			}
+		}
+		else if (y0>=-1 && y0<=1)
+		{
+			// hit the cylinder bit
+			if (t0<=0) return false;
+		
+			out_Response.mHitPosition = start + (direction*t0);
+			out_Response.mNormal = DVector3(out_Response.mHitPosition.mComponent[0], 0, out_Response.mHitPosition.mComponent[2]);
+			out_Response.mNormal.Normalise();
 			return true;
 		}
-	}
-	else if (y0>=-1 && y0<=1)
-	{
-		// hit the cylinder bit
-		if (t0<=0) return false;
-		
-		out_Response.mHitPosition = start + (direction*t0);
-		out_Response.mNormal = DVector3(out_Response.mHitPosition.mComponent[0], 0, out_Response.mHitPosition.mComponent[2]);
-		out_Response.mNormal.Normalise();
-		return true;
-	}
-	else if (y0>1)
-	{
-		if (y1>1)
-			return false;
-		else
+		else if (y0>1)
 		{
-			// hit the cap
-			float th = t0 + (t1-t0) * (y0-1) / (y0-y1);
-			if (th<=0) return false;
+			if (y1>1)
+				return false;
+			else
+			{
+				// hit the cap
+				float th = t0 + (t1-t0) * (y0-1) / (y0-y1);
+				if (th<=0) return false;
 
-			out_Response.mHitPosition = start + (direction*th);
-			out_Response.mNormal = DVector3(0, 1, 0);
+				out_Response.mHitPosition = start + (direction*th);
+				out_Response.mNormal = DVector3(0, 1, 0);
+				return true;
+			}
+		}
+	}
+	else // backface
+	{
+		if (y1<-1)
+		{
+			if (y0<-1)
+				return false;
+			else
+			{
+				// hit the cap
+				float th = t0 + (t1-t0) * (y0+1) / (y0-y1);
+				if (th<=0) return false;
+		
+				out_Response.mHitPosition = start + (direction*th);
+				out_Response.mNormal = DVector3(0, 1, 0);
+				return true;
+			}
+		}
+		else if (y1>=-1 && y1<=1)
+		{
+			// hit the cylinder bit
+			if (t1<=0) return false;
+		
+			out_Response.mHitPosition = start + (direction*t1);
+			out_Response.mNormal = DVector3(-out_Response.mHitPosition.mComponent[0], 0, -out_Response.mHitPosition.mComponent[2]);
+			out_Response.mNormal.Normalise();
 			return true;
+		}
+		else if (y1>1)
+		{
+			if (y0>1)
+				return false;
+			else
+			{
+				// hit the cap
+				float th = t0 + (t1-t0) * (y0-1) / (y0-y1);
+				if (th<=0) return false;
+
+				out_Response.mHitPosition = start + (direction*th);
+				out_Response.mNormal = DVector3(0, -1, 0);
+				return true;
+			}
 		}
 	}
 
